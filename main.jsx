21f10286e858b37f0329847762adba8a3285c0df
efacocirhsoj <107910291+efacocirhsoj@users.noreply.github.com>
2025-01-02 14:48:25 +0800

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ProductList from './pages/ProductList';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ProductList />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
